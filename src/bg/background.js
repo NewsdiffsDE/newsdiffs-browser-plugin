ae4d6e9c4b7a1d6bd882f1270578afbf6b7fdd14
Moreno Gummich <moreno.gummich@gmail.com>
2015-06-24 15:03:19 +0200

    - added background.js - displays the icon if the url is available at newsdiffs.de

--- src/bg/background.js
+++ src/bg/background.js
@@ -6,8 +6,31 @@
 
 
 //example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
+/*chrome.extension.onMessage.addListener(
+ function(request, sender, sendResponse) {
+ chrome.pageAction.show(sender.tab.id);
+ sendResponse();
+ });*/
+
+var runtime = chrome.runtime && chrome.runtime.sendMessage ?
+    'runtime' : 'extension';
+
+var urls = {};
+
+chrome.pageAction.onClicked.addListener(function(tab) {
+  chrome.tabs.create({
+    'index': tab.index + 1,
+    'url': urls[tab.id]
   });
+});
+
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+  delete urls[tabId];
+});
+
+chrome[runtime].onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.action === 'lookup_page') {
+    chrome.pageAction.show(sender.tab.id);
+    // urls[sender.tab.id] = request.url;
+  }
+});
